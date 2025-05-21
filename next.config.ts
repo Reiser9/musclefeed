9997a9dc22f88a612c13c39b29ae3e8b9b681031
Reiser9 <elmanamirhanov@mail.ru>
2025-05-21 18:51:41 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
+
 import type { NextConfig } from 'next';
 import path from 'path';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin('./request.ts');
+const withNextIntl = createNextIntlPlugin("./request.ts");
 
 const nextConfig: NextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
     images: {
-        domains: ['resez.ru'],
+        domains: ['resez.ru', '45.147.179.98', 'muscle.resez.ru'],
         unoptimized: true,
     },
     reactStrictMode: false,
-    output: 'standalone',
 };
 
 export default withNextIntl(nextConfig);
